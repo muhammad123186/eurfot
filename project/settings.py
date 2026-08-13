c6250c1ea547d2d7d60eec9b40d9f45aac6e9c59
محمد البطوش <muhammadalbtuosh123@gmail.com>
2026-08-13 14:18:33 +0300

    fix cache path for production

--- project/settings.py
+++ project/settings.py
@@ -140,12 +140,13 @@ MEDIA_ROOT = BASE_DIR / 'media'
 CACHES = {
     "default": {
         "BACKEND": "django.core.cache.backends.filebased.FileBasedCache",
-        "LOCATION": "C:/Users/mu7_is/Desktop/eurofor/project/django_cache",
+        "LOCATION": BASE_DIR / "django_cache",
         "OPTIONS": {
             "MAX_ENTRIES": 10000,
         }
     }
 }
+
 
 
 
