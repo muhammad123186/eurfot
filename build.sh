f0f1ed58b6f0c800e0a86c757b32aee2a4584417
muhammad 123186 <muhammad albtuosh123@gmail.com>
2026-09-26 16:53:36 +0300

    Restore admin creation

--- build.sh
+++ build.sh
@@ -2,3 +2,10 @@
 
 python manage.py collectstatic --noinput
 python manage.py migrate
+
+if [ -n "$DJANGO_SUPERUSER_USERNAME" ] && [ -n "$DJANGO_SUPERUSER_PASSWORD" ]; then
+    python manage.py createsuperuser \
+        --username "$DJANGO_SUPERUSER_USERNAME" \
+        --email "$DJANGO_SUPERUSER_EMAIL" \
+        --noinput
+fi
